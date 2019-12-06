90dcb42d7e6db97a1ecb570d4d7467b28b2e0c03
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-12-06 11:35:34 -0500

    [Add] Created App class and retrieved api data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/players')
+    .then(res => {
+      this.setState({data: res.data});
+    })
+    .catch(err => console.log(err));
+  }
+  
+  render(){
+    console.log(this.state.data);
+    return(
+      <div>
+        my App
+      </div>
+    )
+  }
 }
 
 export default App;
